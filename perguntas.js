b62d3cf0a49d6102b70f1fb5a18cb8cc17702e1b
Edgarson7 <168139274+Edgarson7@users.noreply.github.com>
2024-10-12 13:57:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+criaCartao(
+    'Programação',
+    'O que é uma placa-mãe',
+    'A placa-mãe é a espinha dorsal que une os componentes do computador em um ponto e permite que eles conversem entre si. Sem ela, nenhuma das peças do computador poderia interagir.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor e o maior país do mundo?',
+    'o menor país é o Vaticano, na europa. ele possui 0,44 km². Já o maior país é a Russia ocupando um pedaço da Ásia e da Europa com um território de 17.075.400 km². '
+)
+
+criaCartao(
+    'Biologia',
+    'O que é cromossomos?',
+    'Os cromossomos são estruturas que abrigam o material genético dentro da célula. Em outras palavras, um cromossomo é uma molécula de DNA condensada, que carrega a informação genética herdada dos progenitores.'
+)
+
+criaCartao(
+    'Física',
+    'Quais são as 3 leis de Newton?',
+    '1ª lei de Newton: lei da inércia. 2ª lei de Newton: princípio fundamental da dinâmica. 3ª lei de Newton: lei da ação e reação.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é um adjetivo e um substantivo?',
+    'É substantivo quando serve de nome a algum ser ou objeto. É adjetivo quando classifica um ser ou objeto.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual a distância do planeta Terra para o Sol?',
+    'O Sol está a 149.600.000 km de distância da Terra. A luz do Sol leva cerca de 8,3 minutos para chegar até a Terra, ou seja, nossa estrela está a uma distância de 8,3 minutos-luz de nós.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Foi um estadista e líder militar francês que ganhou destaque durante a Revolução Francesa e liderou campanhas militares durante as Guerras Revolucionárias Francesas. Foi Imperador dos Franceses como Napoleão I de 1804 a 1814'
+)
+
+criaCartao(
+    'Cultura',
+    'Como se diz "estou satisfeito" na China?',
+    '我很滿意'
+)
+
+criaCartao(
+    'Arte',
+    'Quantas profissões Leonardo da Vinci teve?',
+    'Experts estimam entre 12 e 14 as diversas profissões exercidas por ele. O cara é insano!'
+)
+
+criaCartao(
+    'Ciência',
+    'O que o Serjão dos foguetes (Sérgio Sacani) acha sobre o futuro da humanidade?',
+    'Sérgio Sacani comenta: "É complicado isso daí cara..."'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Qual foi a primeira invenção tecnológica no mundo?',
+    'Alguns dizem que é a lança, outros dizem que é o machado, ou o fogo, ou até mesmo a roda. Mas não se tem uma confirmação correta'
+)
+
+criaCartao(
+    'Engenharia',
+    'Qual é o maior edifício já criado?',
+    'Com mais de 828 metros de altura e 163 andares, o Burj Khalifa é o prédio mais alto do mundo. Localizado em Dubai nos Emirados Árabes Unidos'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que o filósofo Sócrates dizia?',
+    'Só sei que nada sei é uma famosa frase atribuída ao filósofo grego Sócrates que significa um reconhecimento da própria ignorância da parte do autor.'
+)
+
+criaCartao(
+    'Gastronomia',
+    'Como beber água?',
+    'pega um copo (opcional), coloca água no recipiente (opcional), depois você vira a água lentamente para dentro da sua boca, não esqueça de engolir. pronto, você bebeu água. '
+)
+
+criaCartao(
+    'Obscuridade',
+    'Como sequestrar alguém sem ninguém saber...',
+    'É sério que você queria a resposta? Que feio...'
+)
